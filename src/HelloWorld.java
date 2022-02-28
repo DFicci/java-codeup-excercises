91d305d7c9aac492fa5a9cd9b24294c1ea1d17d9
daniel ficci <danielficci@gmail.com>
2022-02-28 14:20:34 -0600

    first set of java excercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,69 @@ public class HelloWorld {
 
     public static void main(String[] args){
         System.out.println("Hello World");
+
+
+        int MyFavoriteNumber = 17;
+
+        System.out.println(MyFavoriteNumber);
+
+        String MyString = "Daniel";
+
+        System.out.println(MyString);
+
+
+        MyString = "D";
+        System.out.println(MyString);
+
+        MyString = "3.14159";
+        System.out.println(MyString);
+
+        float MyNumber = 3.14F;
+        System.out.println(MyNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        class x = 5;
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//
+//        System.out.println(three);
+
+//        int three = (int) "three";
+//        System.out.println(three);
+
+//        int x = 4;
+//        x = x + 5;
+//        System.out.println(x);
+//
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//
+//        System.out.println(y);
+//        System.out.println(x);
+
+//byte z = 1200;
+//z++;
+//System.out.println(z);
+
     }
 }
 
