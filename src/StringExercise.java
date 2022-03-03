351a8d1d7576532378a0ecbda59c2e123c7a3daf
daniel ficci <danielficci@gmail.com>
2022-03-03 10:46:38 -0600

    added bob and String exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,20 @@
+package main;
+
+public class StringExercise {
+    public static void main(String[] args){
+        String pinkFloydQuote = "we don't need no education\nWe dont need no thought control";
+        System.out.println(pinkFloydQuote);
+
+        String quotes = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(quotes);
+
+        String windows = "In windows, the main drive is usually C:\\";
+        System.out.println(windows);
+
+        String backSlash ="I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(backSlash);
+
+
+    }
+}
---
+++ src/bob.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class bob {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("let's talk to bob");
+
+        String userInput = sc.nextLine().trim();
+
+        System.out.println(userInput);
+
+        if (userInput.endsWith("?")){
+            System.out.println("sure");
+        } else if (userInput.endsWith("!")){
+            System.out.println("Whoa, Chill out!");
+        } else if (userInput.equals("")){
+            System.out.println("Fine. be that way.");
+        } else {
+            System.out.println("Whatever.");
+        }
+    }
+}
